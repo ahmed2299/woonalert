681700567d0dd0b695f1f074bbca072c49ca2df0
ahmed2299 <a7mad.hossam2299@gmail.com>
2024-07-26 01:53:44 +0300

    updating schedule_run

--- schedule_run.py
+++ schedule_run.py
@@ -65,8 +65,8 @@ def upload_to_mongo(json_filename, collection_name):
         logging.error(f"Error uploading to MongoDB: {e}")
 
 # Schedule the scripts to run at the same time
-schedule.every().day.at("01:52").do(run_scrapy_script1)
-schedule.every().day.at("01:52").do(run_scrapy_script2)
+schedule.every().day.at("02:00").do(run_scrapy_script1)
+schedule.every().day.at("02:00").do(run_scrapy_script2)
 
 def run_scheduler():
     while True:
