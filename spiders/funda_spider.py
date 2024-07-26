97fd6cde3e877dff345681f2ceb00561d6ddcdd0
ahmed2299 <a7mad.hossam2299@gmail.com>
2024-07-26 15:34:15 +0300

    fix custom settings v1.4 adding retry on status code 200

--- spiders/funda_spider.py
+++ spiders/funda_spider.py
@@ -20,7 +20,7 @@ class FundaSpider(scrapy.Spider):
         'ROBOTSTXT_OBEY': False,
         'RETRY_ENABLED': True,
         'RETRY_TIMES': 5,  # Number of retries
-        'RETRY_HTTP_CODES': [500, 502, 503, 504, 522, 524, 408, 429],
+        'RETRY_HTTP_CODES': [200, 500, 502, 503, 504, 522, 524, 408, 429],
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'REQUEST_FINGERPRINTER_IMPLEMENTATION' : "2.7",
         'CONCURRENT_REQUESTS': 16,
